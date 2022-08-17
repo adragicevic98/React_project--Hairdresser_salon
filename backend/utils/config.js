3bd08a12aa3089991fde17b96f20cbb5b51f462d
Antonela Dragičević <adragice1@pmfst.hr>
2022-08-17 20:33:49 +0200

    Dodane upute za pokretanje

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -1,11 +1,11 @@
 require('dotenv').config() //ucitavanje environment varijabli prilikom pokretanja da bi bile dostupne u objektu process.env u aplikaciji
 
-const PORT=process.env.PORT || 5000
+const PORT=process.env.PORT 
 
 //Baza podataka
 const password=process.env.ATLAS_PASS
 
 const dbname=process.env.NODE_ENV ==='test' ? 'rezervacije-api-test' : 'rezervacije-api'
-const DB_URI=`mongodb+srv://oarwa-ad:${password}@cluster0.xkrn6.mongodb.net/${dbname}?retryWrites=true&w=majority`
-
+//const DB_URI=`mongodb+srv://oarwa-ad:${password}@cluster0.xkrn6.mongodb.net/${dbname}?retryWrites=true&w=majority`
+const DB_URI=`mongodb+srv://goran:${password}@cluster0.2zhnwcs.mongodb.net/${dbname}?retryWrites=true&w=majority`
 module.exports={PORT,DB_URI}
